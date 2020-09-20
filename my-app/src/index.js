5381cdde90156862498048fdfe990f39ae5a1a23
Bridgit Arika <bridgitmob@gmail.com>
2020-09-20 18:48:15 -0500

    i have my first element for the react app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const myfirstelement = (
+  <div>
+    <h2>Welcome to React Transportation</h2> 
+    <p>The best place to buy vehicles online</p>
+
+    <h2>Choose Options</h2>
+  </div>
+
+)
+
+
+
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  myfirstelement,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
