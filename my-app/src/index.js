f00d90be4f1880757d7beab53e9c17e2b5c26c3e
Bridgit Arika <bridgitmob@gmail.com>
2020-09-21 13:51:49 -0500

    making a table

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,54 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const myfirstelement = (
+const myelement = (
   <div>
     <h2>Welcome to React Transportation</h2> 
     <p>The best place to buy vehicles online</p>
 
     <h2>Choose Options</h2>
+    <p>New Only <input type = "checkbox" id = "coding" name = "interest" value = "coding" checked /> </p>
+    <p>Select Type
+      <select>
+        <option value = "All">All</option>
+        <option value = "Cars">Cars</option>
+        <option value = "Trucks">Trucks</option>
+        <option value = "Convertibles">Convertibles</option>
+      </select>
+    </p>
+    <h2>Cars</h2>
+    <ul>
+      <table>
+        <tr>
+          <th>Year</th>
+          <th>Model</th>
+          <th>Price</th>
+          <th>Buy</th>
+        </tr>
+        <tr>
+          <td>2013</td>
+          <td>A</td>
+          <td>$32000</td>
+          <td><button>Buy Now</button></td>
+        </tr>
+      </table>
+    </ul>
+    <ul>
+      <table>
+        <tr>
+          <th>Year</th>
+          <th>Model</th>
+          <th>Price</th>
+          <th>Buy</th>
+        </tr>
+        <tr>
+          <td>2011</td>
+          <td>B</td>
+          <td>$4400</td>
+          <td><button>Buy Now</button></td>
+        </tr>
+      </table>
+    </ul>
   </div>
 
 )
@@ -17,7 +59,7 @@ const myfirstelement = (
 
 
 ReactDOM.render(
-  myfirstelement,
+  myelement,
   document.getElementById('root')
 );
 
